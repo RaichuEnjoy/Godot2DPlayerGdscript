3d0aeac8efbd651c96931c576067ca8ca8d940c5
legocoder123 <legocoder123@gmail.com>
2022-11-30 19:53:41 -0600

    new

--- player.gd
+++ player.gd
@@ -2,13 +2,18 @@ extends KinematicBody2D
 
 #change to change speed
 var speed = 6
+var fastSpeed = 9
 var dead = false
+var health = 100
 
 func _ready():
 	#shows player on start
 	show()
 	 
 func _process(delta):
+	#checks if health is 0
+	if health == 0:
+		dead = true
 	#checks input then changes by relative value
 	if Input.is_action_pressed("left"):
 		move_local_x(-speed)
@@ -18,8 +23,14 @@ func _process(delta):
 		move_local_y(-speed)
 	if Input.is_action_pressed("down"):
 		move_local_y(speed)
+	#sprinting
+	if Input.is_action_pressed("fast"):
+		speed = fastSpeed
+	if !Input.is_action_pressed("fast"):
+		speed = 6
 	if dead == true:
 		#put death code here
 		pass
 		
+
 
